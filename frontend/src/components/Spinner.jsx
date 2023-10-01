6bc9334d3ce58eb5e6539fcdbeab95e4a298d549
Sagar Dhaduk <sagardhaduk1@gmail.com>
2023-09-30 21:58:01 -0500

    "Finished ShowBook, created BackButton component"

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,8 +2,8 @@
  
  const Spinner = () => {
    return (
-     <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600'></div>
+     <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-600 '></div>
    )
  }
  
- export default Spinner
+ export default Spinner;
