50cbc27e6664d1c21f7b2cbd8287ad677cc58c1a
Sagar Dhaduk <sagardhaduk1@gmail.com>
2023-09-30 19:59:12 -0500

    "Fixed bug with CSS, Finished Home.jsx"

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -36,10 +36,10 @@ const Home = () => {
       {loading ? (
         <Spinner />
       ) : (
-        <table className='w-full border-seperate border-spacing-2'>
+        <table className='w-full border-separate border-spacing-2'>
           <thead>
             <tr>
-              <th className='border border-slate-600'>No.</th>
+              <th className='border border-slate-600 rounded-md '>No.</th>
               <th className='border border-slate-600 rounded-md'>Title</th>
               <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
               <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
