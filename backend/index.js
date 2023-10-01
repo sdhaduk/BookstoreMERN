83c57dcf5e7b0871940ae31839df457902ff2a43
Sagar Dhaduk <sagardhaduk1@gmail.com>
2023-09-30 19:55:10 -0500

    "Trouble shooting css"

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,17 @@ import bookRoutes from "./routes/bookRoutes.js";
 import cors from "cors";
 
 
-
 const app = express();
 
 app.use(express.json());
-app.use(
-    cors({
-        origin: "http://localhost:5000",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"]
-    })
-);
+app.use(cors());
+// app.use(
+//     cors({
+//         origin: "http://localhost:5000",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         allowedHeaders: ["Content-Type"]
+//     })
+// );
 
 mongoose
     .connect(process.env.MONGODBURL)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import CreateBook from './pages/CreateBook.jsx';
+import ShowBook from './pages/ShowBook.jsx';
+import EditBook from './pages/EditBook.jsx';
+import DeleteBook from './pages/DeleteBook.jsx';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
